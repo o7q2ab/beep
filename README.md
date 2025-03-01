237665320164ea740a66223e37eb2149059aa7c7
o7q2ab <o7q2ab@gmail.com>
2025-03-01 22:10:31 +0200

    many improvements

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # beep
-Create a new Go project.
+
+Beep is designed to create a new Go module or Go package. By default directory name is bebeep and default module name is bebeep too. To change default behavior use ldflags.
+
+## Install
+
+Use default values:
+```sh
+go install github.com/o7q2ab/beep@latest
+```
+
+Change both directory and module names:
+```sh
+go install \
+    -ldflags="-X \
+        'github.com/o7q2ab/beep/config.Dir=notbebeep' \
+        'github.com/o7q2ab/beep/config.Mod=notnotbebeep' \
+    " \
+    github.com/o7q2ab/beep@latest
+```
+
+## Usage
+
+Create Go module:
+
+```sh
+beep
+```
+
+Create Go pacakge:
+
+```sh
+beep pkg
+```
--- main.go
+++ main.go
@@ -1,47 +1,74 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-)
+	"strconv"
 
-var (
-	Dir = "bebeep"
-	Mod = "bebeep"
+	"github.com/o7q2ab/beep/config"
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := run(os.Args[1:]); err != nil {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(args []string) error {
+	createModule := len(args) == 0 || (len(args) == 1 && args[0] != "pkg")
 	wd, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("getwd: %w", err)
 	}
-	fmt.Println("Beeping in", wd)
-	if err = os.Mkdir(Dir, os.ModePerm); err != nil {
+
+	fmt.Printf("Beeping in %s. ", wd)
+
+	dir, err := mkdir(config.Dir)
+	if err != nil {
 		return fmt.Errorf("mkdir: %w", err)
 	}
-	cmd := exec.Command("go", "mod", "init", Mod)
-	cmd.Dir = filepath.Join(wd, Dir)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("run: %w", err)
+
+	fmt.Printf("Directory %s.\n", dir)
+
+	fullpath := filepath.Join(wd, dir)
+
+	if createModule {
+		cmd := exec.Command("go", "mod", "init", config.Mod)
+		cmd.Dir = fullpath
+		out, err := cmd.CombinedOutput()
+		if len(out) != 0 {
+			fmt.Println(string(out))
+		}
+		if err != nil {
+			return fmt.Errorf("run: %w", err)
+		}
 	}
-	if strout := string(out); strout != "" {
-		fmt.Println(strout)
-	}
-	mainfile := filepath.Join(wd, Dir, "main.go")
+
+	mainfile := filepath.Join(fullpath, "main.go")
 	if err = os.WriteFile(mainfile, []byte(file), 0644); err != nil {
 		return fmt.Errorf("writefile: %w", err)
 	}
 	return nil
+}
+
+func mkdir(name string) (string, error) {
+	for i := 0; ; i++ {
+		if i != 0 {
+			name = name + strconv.Itoa(i)
+		}
+		err := os.Mkdir(name, os.ModePerm)
+		if err == nil {
+			return name, nil
+		}
+		if errors.Is(err, os.ErrExist) {
+			continue
+		}
+		return "", err
+	}
 }
 
 const file = `package main
